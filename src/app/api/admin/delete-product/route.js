a6624997f0587786b649b9824a08b65d692e4480
Razi Benvidi <raziyehes859@gmail.com>
2024-07-14 18:26:17 -0400

    Delete button functionality added

--- src/app/api/admin/delete-product/route.js
+++ src/app/api/admin/delete-product/route.js
@@ -1,4 +1,7 @@
 import { NextResponse } from "next/server";
+import connectToDB from "@/database";
+// import AuthUser from "@/middleware/AuthUser";
+import Product from "@/models/product";
 
 export const dynamic = "force-dynamic";
 
--- src/components/CommonListing/ProductButton/index.js
+++ src/components/CommonListing/ProductButton/index.js
@@ -4,14 +4,32 @@ import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { useContext } from "react";
 import { GlobalContext } from "@/context";
+import { deleteProduct } from "@/services/product";
+import { useState } from "react";
 
 export default function ProductButton({ item }) {
   const pathName = usePathname();
 
   const contextValue = useContext(GlobalContext);
   const setCurrentUpdatedProduct = contextValue.setCurrentUpdatedProduct;
+  const { setComponentLevelLoader, componentLevelLoader } =
+    useContext(GlobalContext);
   const isAdminView = pathName.includes("admin-view");
   const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  async function handleDeleteProduct(item) {
+    const res = await deleteProduct(item._id);
+    if (res && res.success) {
+      setComponentLevelLoader({ loading: true, id: item._id });
+      setErrorMessage(res.message);
+      router.refresh();
+    } else {
+      setComponentLevelLoader({ loading: false, id: "" });
+      setErrorMessage(res.message);
+      console.log(res.message);
+    }
+  }
 
   return isAdminView ? (
     <>
@@ -26,10 +44,21 @@ export default function ProductButton({ item }) {
         Update
       </button>
       <button
+        onClick={() => handleDeleteProduct(item)}
         className="mt-1.5 flex w-full justify-center bg-orange-600  text-black font-semibold  
                 py-2 px-4 border rounded px-5 py-3 text-base uppercase tracking-wide "
       >
-        Delete
+        {componentLevelLoader &&
+        componentLevelLoader.loading &&
+        item._id === componentLevelLoader.id ? (
+          <componentLevelLoader
+            text={"Deleting Product..."}
+            color={"#ffffff"}
+            loading={componentLevelLoader && componentLevelLoader.loading}
+          />
+        ) : (
+          "DELETE"
+        )}
       </button>
     </>
   ) : (
