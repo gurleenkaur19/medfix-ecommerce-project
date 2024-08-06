89d4eb984da90c0f6922c55f0e27e32c590db340
Gurleen Kaur <gurleenkaurkhurpa19@gmail.com>
2024-08-05 20:11:22 -0400

    authentication removed

--- src/app/api/cart/delete-from-cart/route.js
+++ src/app/api/cart/delete-from-cart/route.js
@@ -1,5 +1,4 @@
 import connectToDB from "@/database";
-// import { authorizationService } from "@/utils/authVerify";
 import Cart from "@/models/cart";
 import { NextResponse } from "next/server";
 
@@ -9,13 +8,6 @@ export async function DELETE(req) {
   try {
     await connectToDB();
 
-    // const authHeader = req.headers.get("Authorization");
-    // const isAuthUser = await authorizationService(authHeader, [
-    //   "admin",
-    //   "customer",
-    // ]);
-
-    // if (isAuthUser) {
     const { searchParams } = new URL(req.url);
     const id = searchParams.get("id");
     if (!id)
@@ -37,12 +29,6 @@ export async function DELETE(req) {
         message: "Failed to delete Cart item ! Please try again.",
       });
     }
-    // } else {
-    //   return NextResponse.json({
-    //     success: false,
-    //     message: "You are not authenticated",
-    //   });
-    // }
   } catch (error) {
     return NextResponse.json({
       success: false,
