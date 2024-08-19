05af30f63d78381e68cacd51781289e543b37307
Razi Benvidi <raziyehes859@gmail.com>
2024-08-19 19:30:48 -0400

    error fixed

--- src/app/(frontend)/unauthorized-page/page.js
+++ src/app/(frontend)/unauthorized-page/page.js
@@ -1,8 +1,11 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function Unauthorized() {
+  const router = useRouter();
   return (
-    <section className="h-screen bg-gray-200 mt-[80px]">
+    <section className="h-screen bg-gray-200 mt-[80px] text-black">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="mx-auto mt-8 max-w-screen-xl px-4 sm:px-6 lg:px-8 ">
           <div className="bg-white shadow">
@@ -10,6 +13,13 @@ export default function Unauthorized() {
               <h1 className="font-bold text-lg">
                 You don't have access to view this page!
               </h1>
+              <button
+                type="button"
+                className="bg-transparent hover:bg-red-500 text-red-600 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded"
+                onClick={() => router.push("/")}
+              >
+                Go To Home Page
+              </button>
             </div>
           </div>
         </div>
